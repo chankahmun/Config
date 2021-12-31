6a8440dd68a5c1feb619a8418d0453dd227ef007
chankahmun <45312550+chankahmun@users.noreply.github.com>
2021-12-31 21:25:53 +0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,15 @@
+{
+   "data": {
+      "api": {
+         "url": [
+            "https://financialmodelingprep.com/",
+         ],
+		 "key": [
+			"0f0913e59fe727e1fd0432b590f0a3d4"
+		 ]
+      }
+	  
+	
+		  
+   }
+}
